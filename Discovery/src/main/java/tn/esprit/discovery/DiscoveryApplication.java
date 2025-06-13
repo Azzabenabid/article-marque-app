0f344c9ef046cc213041f6b3fe8c8cc357f82518
azza2000 <79521762+Azzabenabid@users.noreply.github.com>
2025-06-13 22:37:07 +0100

    ajour les dockerfils et le dockercompose

--- Discovery/src/main/java/tn/esprit/discovery/DiscoveryApplication.java
+++ Discovery/src/main/java/tn/esprit/discovery/DiscoveryApplication.java
@@ -2,9 +2,11 @@ package tn.esprit.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DiscoveryApplication {
 
